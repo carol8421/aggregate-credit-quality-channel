88e0e273db668812b426ea3e20884d5dbc11897d
523506791@qq.com <matao@123>
2019-03-26 16:34:44 +0800

    add mongodb properties

---
+++ aggregate-platform-server/src/main/java/com/aggregate/framework/web/Message.java
@@ -0,0 +1,19 @@
+package com.aggregate.framework.web;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2017/12/13.
+ */
+
+
+@Data
+public class Message {
+
+    private String messageCode;
+
+    private String messageStatus;
+
+    private String cause;
+
+}
